393d01832bc2a2479911e9fa6e393cbdf6b10b12
Praise Tompane <praisetompane@icloud.com>
2024-11-23 21:19:00 +0200

    feat: add LEAN 4

---
+++ lean.sh
@@ -0,0 +1,3 @@
+echo "installing Lean 4"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/leanprover-community/mathlib4/master/scripts/install_macos.sh)" && source ~/.profile
+echo "done"
