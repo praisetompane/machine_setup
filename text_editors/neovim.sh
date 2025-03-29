0a3db791d7729ecc4dffed9557e1a4df9747fc98
Praise Tompane <praisetompane@icloud.com>
2025-03-29 12:06:55 +0200

    fix: add lazyvim

--- text_editors/neovim.sh
+++ text_editors/neovim.sh
@@ -1,3 +1,19 @@
 echo "Installing neovim"
     brew install neovim
 echo "Done Installing neovim"
+
+
+echo "Instlling LazyVim"
+rm -rf ~/.config/nvim/
+
+# required
+mv -f ~/.config/nvim{,.bak}
+
+# optional but recommended
+mv -f ~/.local/share/nvim{,.bak}
+mv -f ~/.local/state/nvim{,.bak}
+mv -f ~/.cache/nvim{,.bak}
+
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+
+rm -rf ~/.config/nvim/.git
